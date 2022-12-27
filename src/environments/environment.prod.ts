0e772acd93f53b3c0a16ea63857ec02c98f82ac0
Ruben <ralonzo@cyberwarrior.com>
2022-12-27 09:24:08 -0400

    Add environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBzh7dtZLN-HwRDg9DW6b5_ybQq1zwJ9Zg",
+    authDomain: "clips-50e2b.firebaseapp.com",
+    projectId: "clips-50e2b",
+    storageBucket: "clips-50e2b.appspot.com",
+    // messagingSenderId: "368614559226",
+    appId: "1:368614559226:web:d7595d26177a0af1a02843",
+    measurementId: "G-Z8N55EH8VE"
+  }
+};
