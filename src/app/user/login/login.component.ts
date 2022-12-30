a68bbc188f72af6699caba58f0d6885c6845b013
Ruben <ralonzo@cyberwarrior.com>
2022-12-30 11:27:24 -0400

    Add login feature and dispose modal

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,32 @@ export class LoginComponent {
     password: ''
   }
 
-  login(){
-    console.log("Logging in");
+  showAlert = false
+  alertMsg = ''
+  alertColor = 'blue'
+  inSubmission = false
+
+  constructor(private auth: AngularFireAuth){}
+
+  async login(){
+    try {
+      this.showAlert = true
+      this.alertMsg = 'Logging in...'
+      this.alertColor = 'blue'
+      this.inSubmission = true
+      
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+
+    } catch (e) {
+      console.error(e)
+      this.alertMsg = 'An unexpected error occurred. Please try again later'
+      this.alertColor = 'red'
+      this.inSubmission = false
+      return
+    }
+
+    this.alertMsg = 'Success! You are logged in'
+    this.alertColor = 'green'
+    this.inSubmission = false
   }
 }
