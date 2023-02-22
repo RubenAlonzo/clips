30db34bf11271a7da6894ecc0ccdc83147487dee
Ruben <ralonzo@cyberwarrior.com>
2023-02-22 07:25:42 -0400

    Add unit testing

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,23 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { environment } from 'src/environments/environment';
 
 import { NavComponent } from './nav.component';
+import { RouterModule } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthService', 
+  [ 'createUser', 'logout'], 
+  {
+    isAuthenticated$: of(true),
+  })
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      providers: [
+        { provide: AuthService, useValue: mockedAuthService},
+        { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+      ],
+      imports: [ RouterTestingModule]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it('should logout', () => {
+    const logoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'))
+    
+    expect(logoutLink).withContext('Not logged in').toBeTruthy();
+
+    logoutLink.triggerEventHandler('click')
+
+    const service = TestBed.inject(AuthService)
+    expect(service.logout)
+    .withContext('could not clicked logout ling')
+    .toHaveBeenCalledTimes(1);
+  })
 });
